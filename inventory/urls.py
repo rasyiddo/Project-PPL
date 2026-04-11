46c8c1e24b809be906dde8f54226bda67ad31b4b
Kevin Tanjung <kevin.tanjung@traveloka.com>
2026-04-11 17:03:33 +0700

    feat(product): implement add/edit/list product

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
+from . import views
 from .views import dashboard
 
 urlpatterns = [
+    path('products/', views.product_list, name='product_list'),
+    path('products/create/', views.product_create, name='product_create'),
+    path('products/<int:pk>/edit/', views.product_update, name='product_update'),
     path('', dashboard, name='dashboard'),
 ]
