35b22fa9cc206278196740aead85a17c55326c8a
Kevin Tanjung <kevin.tanjung@traveloka.com>
2026-04-11 14:15:44 +0700

    feat: create Product, InventoryTransaction, InventoryRequest, ProcurementRequest

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from inventory.models import Product, InventoryRequest, ProcurementRequest, InventoryTransaction
+
+admin.site.register(Product)
+admin.site.register(InventoryTransaction)
+admin.site.register(ProcurementRequest)
+admin.site.register(InventoryRequest)
