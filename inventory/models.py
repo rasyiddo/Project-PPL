c2877f72bb93abca3d754a5613d2580058428a6b
Kevin Tanjung <kevintanjung@outlook.com>
2026-04-25 13:27:44 +0700

    feat: implement add/list inventory request (#6)

--- inventory/models.py
+++ inventory/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     stock = models.IntegerField(default=0)
@@ -13,10 +14,14 @@ class Product(models.Model):
             self.name = self.name.upper().strip()
         super().save(*args, **kwargs)
 
+    def __str__(self):
+        return f"{self.name} (Stock: {self.stock})"
+
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['name'], name='unique_product_name')
         ]
+
 
 class InventoryTransaction(models.Model):
     TRANSACTION_TYPE = [
@@ -31,6 +36,7 @@ class InventoryTransaction(models.Model):
     procurement_request = models.ForeignKey('ProcurementRequest', on_delete=models.SET_NULL, null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 class InventoryRequest(models.Model):
     class Meta:
@@ -48,6 +54,7 @@ class InventoryRequest(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     product_name = models.CharField(max_length=200, null=True, blank=True)
     quantity = models.IntegerField(default=0)
+    reason = models.CharField(max_length=200, null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='created_inventory_request')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -57,10 +64,10 @@ class InventoryRequest(models.Model):
     rejected_at = models.DateTimeField(null=True, blank=True)
     rejected_reason = models.CharField(max_length=200, null=True, blank=True)
 
-
     def clean(self):
         if not self.product and not self.product_name:
             raise ValidationError('Please provide either a product or a product name')
+
 
 class ProcurementRequest(models.Model):
     class Meta:
--- inventory/urls.py
+++ inventory/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('products/create/', views.product_create, name='product_create'),
     path('products/<int:pk>/edit/', views.product_update, name='product_update'),
+    path('requests/', views.inventory_request_list, name='inventory_request_list'),
+    path('requests/create/', views.inventory_request_create, name='inventory_request_create'),
     path('', dashboard, name='dashboard'),
 ]
